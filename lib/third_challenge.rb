4d3f46a1175c953494cba39ec88985e8f1bf394c
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-21 03:50:32 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,10 +1,12 @@
+required "pry"
 def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {name:"Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name:"Romeo",:age:"15", status: "alive"},
+          :hero => {name:"Romeo",:age:"15", status: "alive"},
       :hero_friends => []
+       binding.pry
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
