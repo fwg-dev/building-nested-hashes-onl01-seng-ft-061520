6abfe2bc1fd001fc38c38d9ffc3cf84bd44a7aa8
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-21 04:08:00 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,7 +23,7 @@ def bonus
   #code your solution here:
 
 
-epic_tragedy[:montague][:hero][:status] = "alive =dead"
+epic_tragedy[:montague][:hero][:status] = "alive == dead"
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
